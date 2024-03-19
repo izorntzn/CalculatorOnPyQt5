bb4c6378f7b5bc9ba6f38c8cf420c759a7d3481d
izorntzn <tzi.izi@mail.ru>
2024-03-19 21:13:35 +0800

    ,

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
 import sys
+from math import sqrt
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from calculator import Ui_MainWindow
 
@@ -17,32 +19,92 @@ class Calculator(QMainWindow):
         self.ui.pushButton_7.clicked.connect(lambda: self.set_num(7))
         self.ui.pushButton_8.clicked.connect(lambda: self.set_num(8))
         self.ui.pushButton_9.clicked.connect(lambda: self.set_num(9))
+        self.ui.pushButton_18.clicked.connect(lambda: self.set_num('.'))
         self.ui.pushButton_15.clicked.connect(lambda: self.set_num(0))
         self.ui.pushButton_12.clicked.connect(self.devision)
+        self.ui.pushButton_13.clicked.connect(self.multiply)
         self.ui.pushButton_10.clicked.connect(self.equal)
+        self.ui.pushButton_14.clicked.connect(self.plus)
+        self.ui.pushButton_11.clicked.connect(self.minus)
+        self.ui.pushButton_16.clicked.connect(self.square)
+        self.ui.pushButton_17.clicked.connect(self.sqrt)
+        self.ui.pushButton_19.clicked.connect(self.clear)
 
     def set_num(self, num):
-
         self.curnum = self.ui.label.text()
         self.num = str(num)
 
-        if str(self.curnum) != '0':
-            self.ui.label.setText(self.curnum + self.num)
+        if self.curnum != '0':
+            self.curnum = self.ui.label.text()
+            n = self.curnum.count('.')
+            if n == 1 and self.num == '.':
+                pass
+            else:
+                self.ui.label.setText(self.curnum + self.num)
 
         else:
             self.ui.label.clear()
             self.ui.label.setText(self.num)
+
+    def clear(self):
+        self.ui.label.clear()
 
     def devision(self):
         self.f_num = self.ui.label.text()
         self.ui.label.clear()
         self.math = 'devision'
 
+    def multiply(self):
+        self.f_num = self.ui.label.text()
+        self.ui.label.clear()
+        self.math = 'multiply'
+
+    def plus(self):
+        self.f_num = self.ui.label.text()
+        self.ui.label.clear()
+        self.math = 'plus'
+
+    def minus(self):
+        self.f_num = self.ui.label.text()
+        self.ui.label.clear()
+        self.math = 'minus'
+
+    def square(self):
+        self.f_num = self.ui.label.text()
+        ans = int(self.f_num) ** 2
+        self.ui.label.setText(str(ans))
+
+    def sqrt(self):
+        self.f_num = self.ui.label.text()
+        ans = math.sqrt(int(self.f_num))
+        self.ui.label.setText(str(ans))
+
+    def comm(self):
+        # self.curnum = self.ui.label.text()
+        # if '.' not in self.curnum:
+        self.ui.label.setText('.')
+#         if not self.f_num:
+#             self.f_num = int(self.curnum)
+#         else:
+#             self.num = int(self.curnum)
+#             self.f_num = self.f_num + self.num
     def equal(self):
         self.t_num = self.ui.label.text()
 
         if self.math == 'devision':
             ans = int(self.f_num) / int(self.t_num)
+            self.ui.label.setText(str(ans))
+
+        elif self.math == 'multiply':
+            ans = int(self.f_num) * int(self.t_num)
+            self.ui.label.setText(str(ans))
+
+        elif self.math == 'minus':
+            ans = int(self.f_num) - int(self.t_num)
+            self.ui.label.setText(str(ans))
+
+        elif self.math == 'plus':
+            ans = int(self.f_num) + int(self.t_num)
             self.ui.label.setText(str(ans))
 
 
