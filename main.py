c0598b1aa10f3e4fbfb032d087507f24333f4458
izorntzn <tzi.izi@mail.ru>
2024-03-26 22:14:00 +0800

    *

--- main.py
+++ main.py
@@ -5,107 +5,121 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from calculator import Ui_MainWindow
 
 
+def Input_file_equal(func):
+    def wrapper(self):
+        equal = func(self)
+        with open("history.txt", "a+") as f:
+            f_input = f'{self.f_num} {self.math} {self.s_num} = {self.ans}'
+            f.write(f'{f_input}\n')
+        return equal
+    return wrapper
+
+def Input_file_other(func):
+    def wrapper(self):
+        other = func(self)
+        with open("history.txt", "a+") as f:
+            f_input = f'{self.math}({self.f_num}) = {self.ans}'
+            f.write(f'{f_input}\n')
+        return other
+    return wrapper
+
 class Calculator(QMainWindow):
     def __init__(self):
         super(Calculator, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.pushButton.clicked.connect(lambda: self.set_num(1))
-        self.ui.pushButton_2.clicked.connect(lambda: self.set_num(2))
-        self.ui.pushButton_3.clicked.connect(lambda: self.set_num(3))
-        self.ui.pushButton_4.clicked.connect(lambda: self.set_num(4))
-        self.ui.pushButton_5.clicked.connect(lambda: self.set_num(5))
-        self.ui.pushButton_6.clicked.connect(lambda: self.set_num(6))
-        self.ui.pushButton_7.clicked.connect(lambda: self.set_num(7))
-        self.ui.pushButton_8.clicked.connect(lambda: self.set_num(8))
-        self.ui.pushButton_9.clicked.connect(lambda: self.set_num(9))
-        self.ui.pushButton_18.clicked.connect(lambda: self.set_num('.'))
-        self.ui.pushButton_15.clicked.connect(lambda: self.set_num(0))
-        self.ui.pushButton_12.clicked.connect(self.devision)
-        self.ui.pushButton_13.clicked.connect(self.multiply)
-        self.ui.pushButton_10.clicked.connect(self.equal)
-        self.ui.pushButton_14.clicked.connect(self.plus)
-        self.ui.pushButton_11.clicked.connect(self.minus)
-        self.ui.pushButton_16.clicked.connect(self.square)
-        self.ui.pushButton_17.clicked.connect(self.sqrt)
-        self.ui.pushButton_19.clicked.connect(self.clear)
+        self.ui.one.clicked.connect(lambda: self.set_num(1))
+        self.ui.two.clicked.connect(lambda: self.set_num(2))
+        self.ui.three.clicked.connect(lambda: self.set_num(3))
+        self.ui.four.clicked.connect(lambda: self.set_num(4))
+        self.ui.five.clicked.connect(lambda: self.set_num(5))
+        self.ui.six.clicked.connect(lambda: self.set_num(6))
+        self.ui.seven.clicked.connect(lambda: self.set_num(7))
+        self.ui.eight.clicked.connect(lambda: self.set_num(8))
+        self.ui.nine.clicked.connect(lambda: self.set_num(9))
+        self.ui.zero.clicked.connect(lambda: self.set_num(0))
+        self.ui.comma.clicked.connect(lambda: self.set_num('.'))
+        self.ui.division.clicked.connect(self.division)
+        self.ui.multiply.clicked.connect(self.multiply)
+        self.ui.equal.clicked.connect(self.equal)
+        self.ui.plus.clicked.connect(self.plus)
+        self.ui.minus.clicked.connect(self.minus)
+        self.ui.square.clicked.connect(self.square)
+        self.ui.sqrt.clicked.connect(self.sqrt)
+        self.ui.deleteAll.clicked.connect(self.delete)
 
     def set_num(self, num):
-        self.curnum = self.ui.label.text()
+        self.curnum = self.ui.inputLabel.text()
         self.num = str(num)
 
         if self.curnum != '0':
-            self.curnum = self.ui.label.text()
+            self.curnum = self.ui.inputLabel.text()
             n = self.curnum.count('.')
             if n == 1 and self.num == '.':
                 pass
             else:
-                self.ui.label.setText(self.curnum + self.num)
+                self.ui.inputLabel.setText(self.curnum + self.num)
 
         else:
-            self.ui.label.clear()
-            self.ui.label.setText(self.num)
+            self.ui.inputLabel.clear()
+            self.ui.inputLabel.setText(self.num)
 
-    def clear(self):
-        self.ui.label.clear()
+    def delete(self):
+        self.ui.inputLabel.clear()
 
-    def devision(self):
-        self.f_num = self.ui.label.text()
-        self.ui.label.clear()
-        self.math = 'devision'
+    def division(self):
+        self.f_num = self.ui.inputLabel.text()
+        self.ui.inputLabel.clear()
+        self.math = '/'
 
     def multiply(self):
-        self.f_num = self.ui.label.text()
-        self.ui.label.clear()
-        self.math = 'multiply'
+        self.f_num = self.ui.inputLabel.text()
+        self.ui.inputLabel.clear()
+        self.math = '*'
 
     def plus(self):
-        self.f_num = self.ui.label.text()
-        self.ui.label.clear()
-        self.math = 'plus'
+        self.f_num = self.ui.inputLabel.text()
+        self.ui.inputLabel.clear()
+        self.math = '+'
 
     def minus(self):
-        self.f_num = self.ui.label.text()
-        self.ui.label.clear()
-        self.math = 'minus'
+        self.f_num = self.ui.inputLabel.text()
+        self.ui.inputLabel.clear()
+        self.math = '-'
 
+    @Input_file_other
     def square(self):
-        self.f_num = self.ui.label.text()
-        ans = int(self.f_num) ** 2
-        self.ui.label.setText(str(ans))
+        self.f_num = self.ui.inputLabel.text()
+        self.ans = int(self.f_num) ** 2
+        self.ui.inputLabel.setText(str(self.ans))
+        self.math = 'square'
 
+    @Input_file_other
     def sqrt(self):
-        self.f_num = self.ui.label.text()
-        ans = math.sqrt(int(self.f_num))
-        self.ui.label.setText(str(ans))
+        self.f_num = self.ui.inputLabel.text()
+        self.ans = math.sqrt(int(self.f_num))
+        self.ui.inputLabel.setText(str(self.ans))
+        self.math = 'sqrt'
 
-    def comm(self):
-        # self.curnum = self.ui.label.text()
-        # if '.' not in self.curnum:
-        self.ui.label.setText('.')
-#         if not self.f_num:
-#             self.f_num = int(self.curnum)
-#         else:
-#             self.num = int(self.curnum)
-#             self.f_num = self.f_num + self.num
+    @Input_file_equal
     def equal(self):
-        self.t_num = self.ui.label.text()
+        self.s_num = self.ui.inputLabel.text()
 
-        if self.math == 'devision':
-            ans = int(self.f_num) / int(self.t_num)
-            self.ui.label.setText(str(ans))
+        if self.math == '/':
+            self.ans = int(self.f_num) / int(self.s_num)
+            self.ui.inputLabel.setText(str(self.ans))
 
-        elif self.math == 'multiply':
-            ans = int(self.f_num) * int(self.t_num)
-            self.ui.label.setText(str(ans))
+        elif self.math == '*':
+            self.ans = int(self.f_num) * int(self.s_num)
+            self.ui.inputLabel.setText(str(self.ans))
 
-        elif self.math == 'minus':
-            ans = int(self.f_num) - int(self.t_num)
-            self.ui.label.setText(str(ans))
+        elif self.math == '-':
+            self.ans = int(self.f_num) - int(self.s_num)
+            self.ui.inputLabel.setText(str(self.ans))
 
-        elif self.math == 'plus':
-            ans = int(self.f_num) + int(self.t_num)
-            self.ui.label.setText(str(ans))
+        elif self.math == '+':
+            self.ans = int(self.f_num) + int(self.s_num)
+            self.ui.inputLabel.setText(str(self.ans))
 
 
 if __name__ == "__main__":
